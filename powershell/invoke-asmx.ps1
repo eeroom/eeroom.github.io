0f8751178b0196b6ce74dbe14191ea8706ace9e1
邓常青 <eedroom@163.com>
2021-06-04 23:31:04 +0800

    powershell

---
+++ powershell/invoke-asmx.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference="Stop";
+$targeturl="http://localhost:49755/Home.asmx";
+$method="POST";
+$action="HelloWorld"
+[string] $reqbody='<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <HelloWorld xmlns="http://tempuri.org/">
+      <a>44</a>
+    </HelloWorld>
+  </soap:Body>
+</soap:Envelope>';
+[System.Net.HttpWebRequest] $req= [System.Net.WebRequest]::Create($targeturl);
+$req.Method=$method;
+$req.ContentType="text/xml; charset=utf-8";
+$req.Headers.Add("SOAPAction","http://tempuri.org/"+$action)
+[byte[]] $bodydata= [System.Text.Encoding]::UTF8.GetBytes($reqbody);
+[System.IO.Stream] $reqStream= $req.GetRequestStream();
+$reqStream.Write($bodydata,0,$bodydata.Length);
+$res= $req.GetResponse();
+[System.IO.Stream] $resStream= $res.GetResponseStream();
+$reader= [System.IO.StreamReader]::new($resStream);
+$xml= $reader.ReadToEnd();
+[System.Console]::WriteLine("响应");
+[System.Console]::WriteLine($xml);
+
+
+
+
+
