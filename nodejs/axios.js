1c445b7ea6e0249185085f0977c9d5e7a9f3a421
eeroom <eedroom@163.com>
2022-03-31 11:21:22 +0800

    精准全局loading,读取response

--- nodejs/axios.js
+++ nodejs/axios.js
@@ -1,15 +1,16 @@
-let axios=require('axios')
+let axios = require('axios')
 /**
  * 注册多个请求拦截器，先注册的后执行
  * 可以多注册几个试一下
  */
-axios.default.interceptors.request.use(request=>{
-    console.log("请求参数",request.data);
-    console.log("请求头",request.headers);
-    //全局loading处理，约定要给特定的请求参数值，读取请求参数，如果有这个特定值，就展示loading的gif图片
-    //添加一个特定的请求头，这样后续响应拦截器去读取这个特定的请求头，如果能读到，就关闭loading的gif图片
+axios.default.interceptors.request.use(request => {
+    console.log("请求参数:", request);
+    console.log("请求头:", request.headers);
+    //如果直接把所有的请求的请求都搞loading，直接使用axios的loading属性就行,但是很多页面没必要搞loading
+    //只有特定的功能操作才有必要搞loading，比如某个耗时提交，或者避免用户重复提交,如果直接用axios的loading，会被其它请求误伤
+    //处理办法：约定特定的请求参数名称和值，读取约定的请求参数和参数值，符合约定，才展示loading的gif图片，如果是数据驱动，就修改那个数据
     return request;//一定要返回一个request;
-},request=>{
+}, request => {
 
 })
 
@@ -17,13 +18,13 @@ axios.default.interceptors.request.use(request=>{
  * 注册多个响应拦截器，先注册的先执行
  * 可以多注册几个试一下
  */
-axios.default.interceptors.response.use(response=>{
-    console.log("响应数据",response.data);
-    console.log("响应头",response.headers);
-    console.log("响应阶段-请求头",response.config.headers);
-    //处理全局loading,读取特定的响应头，如果有，就关闭laoding的gif动画
+axios.default.interceptors.response.use(response => {
+    console.log("响应数据:", response.data);
+    console.log("响应头:", response.headers);
+    console.log("请求信息(请求头和请求数据)-responseinterceptors:", response.config);
+    //精准全局loading,读取response.config.data中约定的请求参数和参数值，符合约定，就关闭laoding的gif动画，如果是数据驱动，就修改那个数据
     return response;//一定要返回数据，这里返回的数据就是后续then方法的参数值
-},response=>{
+}, response => {
 
 })
 
@@ -36,21 +37,23 @@ axios.default.interceptors.response.use(response=>{
  * @param {*} context 包含请求参数，响应结果
  * @returns 
  */
-let createAxiosInterceptor=function (context) {
+let createAxiosInterceptor = function (context) {
     return {
-        apply:function (target,ctx,args) {
-            let cdata=ctx.all?{reqdata:JSON.stringify(args),result:[]}:context;
-            let promise=Reflect.apply(target,ctx,args).then(res=>{
-                cdata.result.push(res);
+        apply: function (target, ctx, args) {
+            let cdata = ctx.all ? { reqdata: JSON.stringify(args), result: [] } : context;
+            let promise = Reflect.apply(target, ctx, args).then(res => {
+                let {status,data,headers:resheader,config={},request}=res;
+                let {headers:reqheader}=config;
+                cdata.result.push(JSON.stringify(request?{status,data,reqheader,resheader}:res));
                 return res;
-            }).catch(err=>{
-                let {message,showmsg=false}=err;
-                console.log("axios请求和响应数据:",cdata);
-                console.log("axios回调方法异常:",err);
-                if(showmsg)
+            }).catch(err => {
+                let { message, showmsg = false } = err;
+                console.log("axios请求和响应数据:", cdata);
+                console.log("axios回调方法异常:", message);
+                if (showmsg)
                     alert(message);
             })
-            promise.then=new Proxy(promise.then,createAxiosInterceptor(cdata));
+            promise.then = new Proxy(promise.then, createAxiosInterceptor(cdata));
             return promise;
         }
     }
@@ -59,42 +62,40 @@ let createAxiosInterceptor=function (context) {
 /**
  * 替换原本的post,get方法，目的是把他们的返回值的promis对象的then方法进行了替换
  */
-axios.default.post=new Proxy(axios.default.post,createAxiosInterceptor());
-axios.default.get=new Proxy(axios.default.get,createAxiosInterceptor());
+axios.default.post = new Proxy(axios.default.post, createAxiosInterceptor());
+axios.default.get = new Proxy(axios.default.get, createAxiosInterceptor());
 
 /**
  * 演示回调函数中的抛异常
  */
- let http=require("http");
+let http = require("http");
 
-
- var server=http.createServer(function(req,res){
-     console.log("服务端，请求url：",req.url);
-     console.log("服务端，请求method：",req.method);
-     console.log("服务端，请求headers：",req.headers);
-    let lstbuffer=[];
-     req.on("data",function (buffer) {
-         lstbuffer.push(buffer);
-     })
-     req.on("end",function (params) {
-         let buffer=Buffer.concat(lstbuffer)
-         console.log("服务端,请求参数",JSON.parse(buffer));
-         res.setHeader("Content-Type","application/json");
-         res.end(JSON.stringify({"aget":12,"name":"张三"}));
-     })
+let httpserver = http.createServer();
+httpserver.on("request", (req, res) => {
+    console.log("服务端，请求url：", req.url);
+    console.log("服务端，请求method：", req.method);
+    console.log("服务端，请求headers：", req.headers);
+    let lstbuffer = [];
+    req.on("data", function (buffer) {
+        lstbuffer.push(buffer);
+    })
+    req.on("end", function (params) {
+        let buffer = Buffer.concat(lstbuffer)
+        console.log("服务端,请求参数", JSON.parse(buffer));
+        res.setHeader("Content-Type", "application/json;charset=utf-8");
+        res.end(JSON.stringify({ "aget": 12, "name": "张三" }),'utf-8');
+    })
 });
-
-let serverport=8091
-server.listen(serverport,"localhost",function(){
+let port = 8091
+httpserver.listen(port, "localhost", function () {
     console.log("开始监听...");
-    console.log("http://localhost:"+serverport);
-    axios.default.post("http://localhost:"+serverport,{"a":3,"b":"张三"})
-    .then(res=>{
+    console.log("http://localhost:" + port);
+    axios.default.post("http://localhost:" + port, { "a": 3, "b": "张三" }).then(res => {
         //todo 业务逻辑代码
         return res.data;
-    }).then(x=>{
+    }).then(x => {
         //todo 业务逻辑代码
-        console.log("响应数据",x.data);
+        console.log("响应数据", x);
         throw new Error("没有请求到预期的数据！")
     })
 });
