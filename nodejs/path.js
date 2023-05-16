08b0e4912efea3ac8a2b51916d81462121d88b6b
eeroom <eedroom@163.com>
2023-05-16 14:28:58 +0800

    nodes的path和fs模块

--- nodejs/path.js
+++ nodejs/path.js
@@ -1,16 +1,26 @@
 let path=require("path")
+let fs= require('fs')
+// console.log(path.join(__dirname,"/a/b.txt"))
+// console.log(path.join(__dirname,"../a/b.txt"))
 
-console.log(path.join(__dirname,"/a/b.txt"))
-console.log(path.join(__dirname,"../a/b.txt"))
+// console.log(path.basename("D:\\Code\\eeroom.github.io\\a\\b.txt",".txt"))
+// console.log(module)
 
-console.log(path.basename("D:\\Code\\eeroom.github.io\\a\\b.txt",".txt"))
-console.log(module)
+let dirpath="D:/02ITCast/spring5高级"
+fs.readdir("D:/02ITCast/spring5高级",(err,files)=>{
+    let pathjson= files.map(x=>({
+        "id": "spring5高级/"+x,
+        "text": x,
+        "children": null,
+        "state": null
+    }))
+    console.log(JSON.stringify(pathjson))
+    fs.writeFileSync("D:/Code/Azeroth.Http/HttpFile/flashplayer/wmvplayer/pathjson.json",JSON.stringify(pathjson))
+})
 
+// module.exports={a:3
+// }
 
-
-module.exports={a:3
-}
-
-exports={
-    b:4
-}
+// exports={
+//     b:4
+// }
